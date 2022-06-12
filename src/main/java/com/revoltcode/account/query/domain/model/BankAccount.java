bea75d380a73cb1d65836497bd1db27d7ea985bc
oddy-bassey <oddybassey@gmail.com>
2022-06-12 19:53:36 +0100

    created account model and an account lookup response  POJO

---
+++ src/main/java/com/revoltcode/account/query/domain/model/BankAccount.java
@@ -0,0 +1,30 @@
+package com.revoltcode.account.query.domain.model;
+
+import com.revoltcode.account.common.dto.AccountType;
+import com.revoltcode.cqrs.core.domain.model.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class BankAccount extends BaseEntity {
+
+    @Id
+    private String id;
+    private String accountHolder;
+    private Date createdDate;
+    private AccountType accountType;
+    private double balance;
+}
+
+
+
---
+++ src/main/java/com/revoltcode/account/query/dto/AccountLookupResponse.java
@@ -0,0 +1,22 @@
+package com.revoltcode.account.query.dto;
+
+import com.revoltcode.account.common.dto.BaseResponse;
+import com.revoltcode.account.query.domain.model.BankAccount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class AccountLookupResponse extends BaseResponse {
+    private List<BankAccount> accounts;
+
+    public AccountLookupResponse(String message){
+        super(message);
+    }
+}
