aaf0b1072363974bfbdd5a705c13dfe726f1053f
oddy-bassey <oddybassey@gmail.com>
2022-06-12 20:15:01 +0100

    implemented the query dispatcher and registered the handler methods

--- src/main/java/com/revoltcode/account/query/QueryApplication.java
+++ src/main/java/com/revoltcode/account/query/QueryApplication.java
@@ -1,13 +1,35 @@
 package com.revoltcode.account.query;
 
+import com.revoltcode.account.query.infrastructure.handler.QueryHandler;
+import com.revoltcode.account.query.query.FindAccountByCustomerIdQuery;
+import com.revoltcode.account.query.query.FindAccountByIdQuery;
+import com.revoltcode.account.query.query.FindAllAccountQuery;
+import com.revoltcode.cqrs.core.infrastructure.dispatcher.QueryDispatcher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
+@RequiredArgsConstructor
 @SpringBootApplication
 public class QueryApplication {
+
+	@Autowired
+	private final QueryDispatcher queryDispatcher;
+
+	@Autowired
+	private final QueryHandler queryHandler;
 
 	public static void main(String[] args) {
 		SpringApplication.run(QueryApplication.class, args);
 	}
 
+	@PostConstruct
+	public void registerHandlers(){
+		queryDispatcher.registerHandler(FindAllAccountQuery.class, queryHandler::handle);
+		queryDispatcher.registerHandler(FindAccountByIdQuery.class, queryHandler::handle);
+		queryDispatcher.registerHandler(FindAccountByCustomerIdQuery.class, queryHandler::handle);
+	}
 }
